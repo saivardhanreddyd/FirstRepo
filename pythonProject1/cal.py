50a9c153397d8562263361ed050e1e8a471327f6
Komireddy Chandu Reddy <saivardhanreddydendi@gmail.com>
2023-07-16 14:55:35 +0100

    intial project commit

---
+++ pythonProject1/cal.py
@@ -0,0 +1,37 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+print("Calculator")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter your choice (1-4): ")
+
+if choice in ['1', '2', '3', '4']:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    else:
+        if num2 != 0:
+            print("Result:", divide(num1, num2))
+        else:
+            print("Error: Cannot divide by zero!")
+else:
+    print("Invalid choice!")
